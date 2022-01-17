01e6671936ed5ff6f44452f1c20bcd89e6db5136
bytedefined <56295147+Bytedefined@users.noreply.github.com>
2022-01-17 21:46:07 +0800

    Improve

--- Update-Doctumentation-Bash.bash
+++ Update-Doctumentation-Bash.bash
@@ -1,3 +1,4 @@
-echo "Creating documentation based on git history "
-
-git log -p --all --pretty=format:'Author : %an %nDate/Time :  %aD%nCommit : %s' > ./documentaion.md
+echo "Creating documentation based on GIT history..."
+rm ./documentation.md
+git log -p --all --pretty=format:'Author : %an %nDate/Time :  %aD%nCommit : %s' > ./documentation.md
+echo "Process completed successfully. You may now close this window."
