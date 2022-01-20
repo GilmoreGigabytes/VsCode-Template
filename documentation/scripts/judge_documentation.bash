9bbc6861b9eb0265cf679226309c6d60968c7d47
Declan Combley <90402120+DeclanB-C@users.noreply.github.com>
2022-01-20 14:14:27 +0800

    Update judge_documentation.bash

--- documentation/scripts/judge_documentation.bash
+++ documentation/scripts/judge_documentation.bash
@@ -1,4 +1,4 @@
 echo "Creating documentation based on GIT history..."
-rm ./documentation.md
+rm ./judgeDocumentation.md
 git log -p --all --pretty=format:'Author : %an %nDate/Time :  %aD%nCommit : %s' > ./documentation.md
 echo "Process completed successfully. You may now close this window."
