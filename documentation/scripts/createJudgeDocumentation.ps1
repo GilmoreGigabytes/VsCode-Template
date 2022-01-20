31007715373f46c2c82aae345c809fb1e5aff48a
Declan Combley <90402120+DeclanB-C@users.noreply.github.com>
2022-01-20 14:38:26 +0800

    Update createJudgeDocumentation.ps1

--- documentation/scripts/createJudgeDocumentation.ps1
+++ documentation/scripts/createJudgeDocumentation.ps1
@@ -1,4 +1,4 @@
 echo "Creating documentaion based on GIT commit history."
-rm ./judgeDocumentation.md
-git log -p --all --pretty=format:'Author : %an %nDate/Time :  %aD%nCommit : %s' > ./documentation.md
+rm ./documentation/files/judgeDocumentation.md
+git log -p --all --pretty=format:'Author : %an %nDate/Time :  %aD%nCommit : %s' > ./documentation/files/judgeDocumentation.md
 echo "Process completed successfully. You may now close this window."
