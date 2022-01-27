ea5ada15849d5ca7888ada4eeca0ce6cb2ef9923
Declan <declan-combley@protonmail.com>
2022-01-27 16:18:57 +0800

    Hopefully fixed the scripts

--- documentation/scripts/createJudgeDocumentation.bash
+++ documentation/scripts/createJudgeDocumentation.bash
@@ -1,4 +1,4 @@
 echo "Creating documentation based on GIT history..."
-rm ./documentation/files/judgeDocumentation.md
+rm ../files/judgeDocumentation.md
 git log -p --all --pretty=format:'Author : %an %nDate/Time :  %aD%nCommit : %s' > ./documentation/files/judgeDocumentation.md
 echo "Process completed successfully. You may now close this window."
--- documentation/scripts/createJudgeDocumentation.ps1
+++ documentation/scripts/createJudgeDocumentation.ps1
@@ -1,4 +1,4 @@
 echo "Creating documentaion based on GIT commit history."
-rm ./documentation/files/judgeDocumentation.md
+rm ../files/judgeDocumentation.md
 git log -p --all --pretty=format:'Author : %an %nDate/Time :  %aD%nCommit : %s' > ./documentation/files/judgeDocumentation.md
 echo "Process completed successfully. You may now close this window."
