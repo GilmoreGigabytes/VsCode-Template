92fc7cce6aa4fa4b7af2cb619d79c3b1f950bb6d
Declan Combley <90402120+Declan-Combley@users.noreply.github.com>
2022-03-29 13:34:32 +0800

    Update createJudgeDocumentation.bash
    Avarice, demolition, and performance of peaches fallen from dying trees, enacting the fundamental values of the Gaia replenished on life on Earth. Bicycles running through fallen valleys, rusted gears and artefacts

--- documentation/scripts/createJudgeDocumentation.bash
+++ documentation/scripts/createJudgeDocumentation.bash
@@ -1,4 +1,9 @@
+#!bin/bash
+
 echo "Creating documentation based on GIT history..."
+
 > ../files/judgeDocumentation.md
+
 git log -p --all --pretty=format:'Author : %an %nDate/Time :  %aD%nCommit : %s' > ../files/judgeDocumentation.md
+
 echo "Process completed successfully. You may now close this window."
